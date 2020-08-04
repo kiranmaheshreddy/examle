bbddfa4ff742ef9d98b4f0eb8ecf6dfc126c47e9
DUVVI KIRAN MAHESH REDDY <32254547+kiranmaheshreddy@users.noreply.github.com>
2020-08-04 10:31:34 +0530

    Add files via upload

---
+++ Javascript course/variables.js
@@ -0,0 +1,18 @@
+var firstName= "Kiran Mahesh";
+var mobileNo= 9573928958;
+var isNew=false;
+var salary=12.65;
+
+console.log(`
+    My Name is: ${firstName}
+    with mobile no: ${mobileNo}
+    and salary: ${salary}
+    and my status is ${isNew}
+
+`);
+
+//var isEven = (num) =>{
+  //  return  num%2===0;
+//}
+
+console.log([2,,4,6,8].every((num)=>(num%2==0)));
